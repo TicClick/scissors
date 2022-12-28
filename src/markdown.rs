TicClick/scissors

commit b3b85495ce757e386733d878a9648d71c32edd30

fix profile regexp being too greedy and matching on nonsense
(that is, capturing everything before the first profile link)

src/markdown.rs
@@ -105,7 +105,7 @@ impl Article<'_> {
         )??\ +?
         \[
             (?P<username_dirty>
-                .+?
+                [a-zA-Z0-9- \]\[]+?
             )
         \]
         \(

commit 7ddd37a29c798f4690f22bc57915e94c82d235f2

fix matching against usernames with escaped characters broken in b3b85495

src/markdown.rs
@@ -105,7 +105,7 @@ impl Article<'_> {
         )??\ +?
         \[
             (?P<username_dirty>
-                [a-zA-Z0-9- \]\[]+?
+                [a-zA-Z0-9- \\_\]\[]+?
             )
         \]
         \(